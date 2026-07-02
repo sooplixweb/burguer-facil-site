16cbeabdb25907b3189211d5d1cb4d2f7dfdbcc5
Anderson Mendes <andersonmendesdesouza2007@gmail.com>
2026-07-02 17:13:02 -0300

    first commit

--- src/service/api.ts
+++ src/service/api.ts
@@ -1,8 +1,9 @@
 import axios from "axios";
 
 export const API_BASE_URL =
-  import.meta.env.VITE_API_URL ||  "https://amburgueria-api.onrender.com";
-  // "http://localhost:3000";
+  import.meta.env.VITE_API_URL ||
+  "https://burguer-facil-api-production.up.railway.app";
+// "http://localhost:3000";
 
 const api = axios.create({
   baseURL: API_BASE_URL,
